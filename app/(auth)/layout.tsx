6c8cf5bfc221d850a6a4159c766bf9dbd3a32d16
Jonathan Cantillo <jccantillog@gmail.com>
2024-11-29 18:27:59 -0500

    feat: Improve auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,19 +5,19 @@ import Image from "next/image";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
+    // eslint-disable-next-line sort-keys
     <Grid container className="h-screen" columns={{ xs: 1, md: 5 }}>
-      <Grid
-        container
-        alignItems="center"
-        className="bg-white"
-        direction="column"
-        size={{ xs: 1, md: 2 }}
-      >
-        <Grid container alignItems="center" className="h-2/5" direction="row">
+      {/* Auth page container */}
+      {/* eslint-disable-next-line sort-keys */}
+      <Grid className="bg-white" size={{ xs: 1, md: 2 }}>
+        <Grid container className="px-5" flexGrow={1} justifyContent="center">
           <Logo />
         </Grid>
-        <Grid className="h-3/5">{children}</Grid>
+        <Grid className="h-full px-5" flexGrow={1}>
+          {children}
+        </Grid>
       </Grid>
+      {/* Shopping cart placeholder */}
       <Grid
         className="relative bg-gradient-to-r from-orange-400 to-orange-500"
         size={{ md: 3 }}
--- app/ui/atoms/Logo.tsx
+++ app/ui/atoms/Logo.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 
 interface LogoProps {
+  height?: number;
   single?: boolean;
+  width?: number;
 }
 
-export default function Logo({ single }: LogoProps) {
+export default function Logo({ height = 500, single, width = 500 }: LogoProps) {
   return single ? (
     <Image alt="Mercapp logo" height={200} src="/single-logo.png" width={200} />
   ) : (
-    <Image alt="Mercapp logo" height={500} src="/logo.png" width={500} />
+    <Image alt="Mercapp logo" height={height} src="/logo.png" width={width} />
   );
 }
