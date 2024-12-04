769012a9bb45efade70b49b8e57f3c3449c83c1b
Jonathan Cantillo <jccantillog@gmail.com>
2024-12-04 16:13:47 -0500

    feat: Add Signup page v1

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,3 +1,52 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
+
 export default function Page() {
-  return <p>SignUp Page</p>;
+  return (
+    <Box className="flex flex-col gap-4" component="div">
+      <Typography variant="h4">Sign Up</Typography>
+      <TextField
+        fullWidth
+        id="name-input"
+        label="Name"
+        size="small"
+        variant="outlined"
+      />
+      <TextField
+        fullWidth
+        id="lastName-input"
+        label="Last name"
+        size="small"
+        variant="outlined"
+      />
+      <TextField
+        fullWidth
+        id="username-input"
+        label="Username"
+        size="small"
+        variant="outlined"
+      />
+      <TextField
+        fullWidth
+        id="email-input"
+        label="Email"
+        size="small"
+        variant="outlined"
+      />
+      <TextField
+        fullWidth
+        id="password-input"
+        label="Password"
+        size="small"
+        variant="outlined"
+      />
+      <TextField
+        fullWidth
+        id="confirmPassword-input"
+        label="Confirm password"
+        size="small"
+        variant="outlined"
+      />
+      <Button variant="contained">Login</Button>
+    </Box>
+  );
 }
