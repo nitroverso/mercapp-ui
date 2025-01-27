7129fdcfc4c3379f301cd8607b68f5cf9d62b259
Jonathan Cantillo <jccantillog@gmail.com>
2025-01-26 20:00:09 -0500

    feat: Implement user login alert

--- app/AppInitializer.tsx
+++ app/AppInitializer.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from "react";
 import { useSession } from "next-auth/react";
+import { useTranslations } from "next-intl";
 // components
 import LanguageSelector from "@/app/ui/components/LanguageSelector";
 import SnackbarAlert from "@/app/ui/components/Alert";
@@ -13,16 +14,24 @@ interface AppInitializerProps {
 }
 
 const AppInitializer = ({ children }: AppInitializerProps) => {
+  const t = useTranslations("ui");
   const { data: session, status } = useSession();
   const {
+    auth: { session: storedSession },
     authActions: { setSession },
+    uiActions: {
+      uiAlertsActions: { setMessage },
+    },
   } = useStore();
 
   useEffect(() => {
-    if (status === "authenticated" && session?.user) setSession(session.user);
+    if (status === "authenticated" && session?.user && !storedSession) {
+      setSession(session.user);
+      setMessage(t("welcome", { name: session.user.profile.firstName }));
+    }
     if (status === "unauthenticated") setSession(null);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [status, session?.user]);
+  }, [status, session?.user, storedSession]);
 
   return (
     <>
---
+++ app/ui/components/Alert.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useTranslations } from "next-intl";
+// components
+import {
+  Alert,
+  AlertTitle,
+  Snackbar,
+  SnackbarCloseReason,
+} from "@mui/material";
+// hooks
+import { useStore } from "@/app/lib/hooks/useStore";
+
+const SnackbarAlert = () => {
+  const t = useTranslations("ui");
+  const {
+    uiAlerts: { message },
+    uiActions: {
+      uiAlertsActions: { setMessage },
+    },
+  } = useStore();
+
+  const [showAlert, setShowAlert] = useState(false);
+
+  useEffect(() => {
+    if (message) setShowAlert(true);
+  }, [message]);
+
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: SnackbarCloseReason
+  ) => {
+    if (reason === "clickaway") return;
+    setShowAlert(false);
+    setMessage("");
+  };
+
+  return (
+    <Snackbar
+      anchorOrigin={{ horizontal: "center", vertical: "bottom" }}
+      autoHideDuration={5000}
+      open={showAlert}
+      onClose={handleClose}
+    >
+      <Alert severity="success" onClose={handleClose}>
+        <AlertTitle>{t("success")}</AlertTitle>
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
+
+export default SnackbarAlert;
